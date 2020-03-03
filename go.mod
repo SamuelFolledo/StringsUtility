39a607709be5d50ff79fee2bf0d52f24eb09c23d
Samuel Folledo <samuelfolledo@gmail.com>
2020-03-03 15:01:45 -0800

    [add] add go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SamuelFolledo/AppDevUtility
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	// "net/http"
+	// "os"
+	"fmt"
+	// "strings"
+)
+
+//app's entry point
+func main() {
+	fmt.Println("Hi")
+}
